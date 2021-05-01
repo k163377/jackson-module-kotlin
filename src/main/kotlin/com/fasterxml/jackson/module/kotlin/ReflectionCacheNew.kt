37cd087df5163014c75f7b3bdd8c43462ed4d2d9
wrongwrong <boranti1995@gmail.com>
2021-05-01 15:34:03 +0900

    add cache

---
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCacheNew.kt
@@ -0,0 +1,24 @@
+package com.fasterxml.jackson.module.kotlin
+
+import com.fasterxml.jackson.databind.util.SimpleLookupCache
+import java.lang.reflect.Constructor
+import java.lang.reflect.Method
+import kotlin.reflect.jvm.kotlinFunction
+
+// TODO: Streamlining the process of getting an Instantiator by reflection.
+internal class ReflectionCacheNew(reflectionCacheSize: Int) {
+    private val javaConstructorToKotlin = SimpleLookupCache<Constructor<Any>, ConstructorInstantiator<Any>>(reflectionCacheSize, reflectionCacheSize)
+    private val javaMethodToKotlin = SimpleLookupCache<Method, MethodInstantiator<*>>(reflectionCacheSize, reflectionCacheSize)
+
+    fun kotlinFromJava(key: Constructor<Any>): ConstructorInstantiator<Any>? = javaConstructorToKotlin.get(key)
+        ?: key.kotlinFunction?.let {
+            val instantiator = ConstructorInstantiator(it)
+            javaConstructorToKotlin.putIfAbsent(key, instantiator) ?: instantiator
+        }
+
+    fun kotlinFromJava(key: Method): MethodInstantiator<*>? = javaMethodToKotlin.get(key)
+        ?: key.kotlinFunction?.let {
+            val instantiator = MethodInstantiator(it)
+            javaMethodToKotlin.putIfAbsent(key, instantiator) ?: instantiator
+        }
+}
