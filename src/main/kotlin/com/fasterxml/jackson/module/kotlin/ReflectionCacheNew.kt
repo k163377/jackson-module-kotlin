ef3da8c0b835253458cc611f7aa7681a97cf2d05
wrongwrong <boranti1995@gmail.com>
2021-05-01 17:48:43 +0900

    fis cache

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCacheNew.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/ReflectionCacheNew.kt
@@ -1,6 +1,6 @@
 package com.fasterxml.jackson.module.kotlin
 
-import com.fasterxml.jackson.databind.util.SimpleLookupCache
+import com.fasterxml.jackson.databind.util.LRUMap
 import java.lang.reflect.Constructor
 import java.lang.reflect.Method
 import kotlin.reflect.jvm.kotlinFunction
@@ -8,8 +8,8 @@ import kotlin.reflect.jvm.kotlinFunction
 // TODO: Streamlining the process of getting an Instantiator by reflection.
 // TODO: add extensionReceiverParameter check
 internal class ReflectionCacheNew(reflectionCacheSize: Int) {
-    private val javaConstructorToKotlin = SimpleLookupCache<Constructor<Any>, ConstructorInstantiator<Any>>(reflectionCacheSize, reflectionCacheSize)
-    private val javaMethodToKotlin = SimpleLookupCache<Method, MethodInstantiator<*>>(reflectionCacheSize, reflectionCacheSize)
+    private val javaConstructorToKotlin = LRUMap<Constructor<Any>, ConstructorInstantiator<Any>>(reflectionCacheSize, reflectionCacheSize)
+    private val javaMethodToKotlin = LRUMap<Method, MethodInstantiator<*>>(reflectionCacheSize, reflectionCacheSize)
 
     fun kotlinFromJava(key: Constructor<Any>): ConstructorInstantiator<Any>? = javaConstructorToKotlin.get(key)
         ?: key.kotlinFunction?.let {
