50e3236a628552b04d7f74c7368d0d183ecbf204
wrongwrong <boranti1995@gmail.com>
2021-05-01 16:03:56 +0900

    add companion check option

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/MethodInstantiator.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/MethodInstantiator.kt
@@ -5,6 +5,7 @@ import com.fasterxml.jackson.databind.MapperFeature
 import java.lang.reflect.Method
 import kotlin.reflect.KFunction
 import kotlin.reflect.KParameter
+import kotlin.reflect.full.instanceParameter
 import kotlin.reflect.full.valueParameters
 import kotlin.reflect.jvm.javaMethod
 
@@ -13,13 +14,28 @@ internal class MethodInstantiator<T>(kFunction: KFunction<T>) : Instantiator<T> 
     override val hasValueParameter: Boolean = true // TODO: fix on support top level function
     override val valueParameters: List<KParameter> = kFunction.valueParameters
     private val method = kFunction.javaMethod!!
-    // TODO: add check or fallback
-    private val instance = method.declaringClass.kotlin.objectInstance!!
+    // TODO: add fallback option
+    private val instance: Any?
     private val accessible: Boolean = method.isAccessible
     private val bucketGenerator = BucketGenerator(valueParameters)
 
     init {
         method.isAccessible = true
+
+        val possibleCompanion = kFunction.instanceParameter!!.type.erasedType().kotlin
+
+        instance = if (!possibleCompanion.isCompanion) {
+            null
+        } else {
+            try {
+                possibleCompanion.objectInstance!!
+            } catch (ex: IllegalAccessException) {
+                val companionField = possibleCompanion.java.enclosingClass.fields.firstOrNull { it.name == "Companion" }
+                    ?: throw ex
+                companionField.isAccessible = true
+                companionField.get(null) ?: throw ex
+            }
+        }
     }
 
     // This initialization process is heavy and will not be done until it is needed.
@@ -41,7 +57,7 @@ internal class MethodInstantiator<T>(kFunction: KFunction<T>) : Instantiator<T> 
             return
         }
 
-        throw IllegalAccessException("Cannot access to Method, instead found ${instance::class.java.name}")
+        throw IllegalAccessException("Cannot access to Method, instead found ${method.name}")
     }
 
     override fun generateBucket() = bucketGenerator.generate()
