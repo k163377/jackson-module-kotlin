9a8a7538e12e3783b50e28621a67c7cb086f3d95
wrongwrong <boranti1995@gmail.com>
2021-05-03 14:34:22 +0900

    add Instantiator interface

---
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/Instantiator.kt
@@ -0,0 +1,17 @@
+package com.fasterxml.jackson.module.kotlin
+
+import com.fasterxml.jackson.databind.DeserializationContext
+import kotlin.reflect.KParameter
+
+internal interface Instantiator<T> {
+    val hasValueParameter: Boolean
+    val valueParameters: List<KParameter>
+    fun checkAccessibility(ctxt: DeserializationContext)
+
+    fun generateBucket(): ArgumentBucket
+    fun callBy(bucket: ArgumentBucket): T
+
+    companion object {
+        val INT_PRIMITIVE_CLASS: Class<Int> = Int::class.javaPrimitiveType!!
+    }
+}
