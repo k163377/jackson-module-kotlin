5cab8200031a516c3e677216aa48f514d55e8227
Drew Stephens <drew@dinomite.net>
2021-05-07 06:42:13 -0400

    Javadoc comments

--- src/main/java/com/fasterxml/jackson/module/kotlin/SpreadWrapper.java
+++ src/main/java/com/fasterxml/jackson/module/kotlin/SpreadWrapper.java
@@ -7,29 +7,39 @@ import java.lang.reflect.Constructor;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-// Wrapper to avoid costly calls using spread operator.
+/**
+ * Wrapper to avoid costly calls using spread operator.
+ */
 class SpreadWrapper {
     public static <T> Constructor<T> getConstructor(
-            @NotNull Class<T> clazz, @NotNull Class<?>[] parameterTypes
+            @NotNull Class<T> clazz,
+            @NotNull Class<?>[] parameterTypes
     ) throws NoSuchMethodException {
         return clazz.getConstructor(parameterTypes);
     }
 
     public static <T> T newInstance(
-            @NotNull Constructor<T> constructor, @NotNull Object[] initargs
+            @NotNull Constructor<T> constructor,
+            @NotNull Object[] initargs
     ) throws InvocationTargetException, InstantiationException, IllegalAccessException {
         return constructor.newInstance(initargs);
     }
 
     public static Method getDeclaredMethod(
-            @NotNull Class<?> clazz, @NotNull String name, @NotNull Class<?>[] parameterTypes
+            @NotNull Class<?> clazz,
+            @NotNull String name,
+            @NotNull Class<?>[] parameterTypes
     ) throws NoSuchMethodException {
         return clazz.getDeclaredMethod(name, parameterTypes);
     }
 
-    // instance is null on static method
+    /**
+     * Instance is null on static method
+     */
     public static Object invoke(
-            @NotNull Method method, @Nullable Object instance, @NotNull Object[] args
+            @NotNull Method method,
+            @Nullable Object instance,
+            @NotNull Object[] args
     ) throws InvocationTargetException, IllegalAccessException {
         return method.invoke(instance, args);
     }
