a2befea1f91d9036a1e4224e763221c5e38e0bdd
wrongwrong <boranti1995@gmail.com>
2021-05-08 13:53:34 +0900

    add comments

--- src/main/kotlin/com/fasterxml/jackson/module/kotlin/Instantiator.kt
+++ src/main/kotlin/com/fasterxml/jackson/module/kotlin/Instantiator.kt
@@ -5,10 +5,27 @@ import kotlin.reflect.KParameter
 
 internal interface Instantiator<T> {
     val hasInstanceParameter: Boolean
+
+    /**
+     * ValueParameters of the KFunction to be called.
+     */
     val valueParameters: List<KParameter>
+
+    /**
+     * Checking process to see if access from context is possible.
+     * @throws  IllegalAccessException
+     */
     fun checkAccessibility(ctxt: DeserializationContext)
 
+    /**
+     * The process of getting the target bucket to set the value.
+     */
     fun generateBucket(): ArgumentBucket
+
+    /**
+     * Function call from bucket.
+     * If there are uninitialized arguments, the call is made using the default function.
+     */
     fun callBy(bucket: ArgumentBucket): T
 
     companion object {
